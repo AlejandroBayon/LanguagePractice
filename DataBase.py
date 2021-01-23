20ee1577ab21bf3a79fd2949d0d95a7fa05716a3
Alejandro Ruiz Bayón <alejandro_bayon@hotmail.com>
2021-01-23 14:35:21 +0100

    Added new method to update database from a file
    A new method is added to update the database from a .txt file. In this file, each row represents a new word to be added, with the following format:
    
    word: definition
    
    The definition can contain letters, numbers and all the symbols except ":".

--- DataBase.py
+++ DataBase.py
@@ -1,4 +1,6 @@
 import json
+import re
+import os
 
 def open_DB():
     with open("DataBase.txt") as file:
@@ -10,7 +12,7 @@ def close_DB(Dic):
     with open("DataBase.txt", "w") as file:
         json.dump(Dic, file)
     
-def add(Dic):
+def add_single(Dic):
     adding = 1
     while adding == 1:
         key = input("\nEnter a new word: ")
@@ -21,3 +23,16 @@ def add(Dic):
             adding = 0
     else:
         return Dic
+
+def add_from_file(Dic):
+    input_file = input("\nFrom which file do you want to add new words? ")
+    if os.path.exists(input_file):
+        with open(input_file) as file:
+            for row in file:
+                new_row = row.strip()
+                key, value = re.split(": ", new_row)
+                Dic[key] = value
+        print("\nNew words added successfully!\n")
+    else:
+        print("\nSorry, this file doesn't exist\n")
+    return Dic
--- Main.py
+++ Main.py
@@ -11,15 +11,18 @@ while executing == 1:
     print("What do you want to do?\n")
     print("Add new words to the database:")
     print("1: add new words one by one")
+    print("2: add new words from a file")
     print("\nPlay games:")
-    print("2: play to 'Spaces'")
-    print("\n3: exit")
+    print("3: play to 'Spaces'")
+    print("\n4: exit")
 
     response = input("\n")
 
     if response == "1":
-        Dic = DataBase.add(Dic)
+        Dic = DataBase.add_single(Dic)
     elif response == "2":
+        Dic = DataBase.add_from_file(Dic)
+    elif response == "3":
         Game1_Spaces.game(Dic)
     else:
         print("\nOk, see you soon!\n")
